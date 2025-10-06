71e023a8ce75f5de1e0c95a58f55ffcdbf990f21
adrien <nordin_a@etna-alterance.net>
2025-10-06 21:40:41 +0200

    Fix: tailwind cleanup

--- web/greencop_front/tailwind.config.js
+++ web/greencop_front/tailwind.config.js
@@ -1,12 +1,17 @@
+// tailwind.config.js
 export default {
+  legacy: {
+    enable: true
+  },
   theme: {
     extend: {
       colors: {
-        primary: '#00b894',   // vert moderne
-        secondary: '#0984e3', // bleu
-        neutral: '#f5f5f5',   // blanc cassé
-        dark: '#2d3436',      // gris profond
-      },
-    },
+        primary: '#00b894',
+        secondary: '#0984e3',
+        neutral: '#f5f5f5',
+        dark: '#2d3436'
+      }
+    }
   },
+  plugins: [],
 }
