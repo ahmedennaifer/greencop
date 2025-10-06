45290e9cf4d913f420e13c690333d8aef76a227c
adrien <nordin_a@etna-alterance.net>
2025-10-06 21:18:48 +0200

    Feat : upgrading tailwind

---
+++ web/greencop_front/src/components/Layout/AppLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex h-screen bg-neutral">
+    <AppSidebar />
+    <div class="flex-1 flex flex-col">
+      <AppHeader />
+      <main class="p-6 overflow-y-auto flex-1">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import AppHeader from './AppHeader.vue'
+import AppSidebar from './AppSidebar.vue'
+</script>
