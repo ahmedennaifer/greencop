48261aea0ca1e949d52b10b1bc3fff807eca7589
adrien <nordin_a@etna-alterance.net>
2025-10-06 21:18:48 +0200

    Feat : upgrading tailwind

---
+++ web/greencop_front/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00b894',   // vert moderne
+        secondary: '#0984e3', // bleu
+        neutral: '#f5f5f5',   // blanc cassé
+        dark: '#2d3436',      // gris profond
+      },
+    },
+  },
+}
